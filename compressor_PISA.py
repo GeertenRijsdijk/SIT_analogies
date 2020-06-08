105d4778593c59c6b680cb8fe6ee8ba5e4356b3c
Geerten Rijsdijk <geertenrijsdijk@hotmail.com>
2020-06-08 10:52:43 +0200

    added file to try PISA compressor standalone

---
+++ compressor_PISA.py
@@ -0,0 +1,16 @@
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('USAGE: compressor_bruteforce.py <code>')
+    elif not sys.argv[1].isalpha():
+        print('Code must contain only letters!')
+    else:
+        from PISA.encode import encode
+        from PISA.graphs import Graph
+        s = sys.argv[1]
+        g = Graph(s)
+        encode(g)
+        code, load = g.edges[g.nodes[0]][g.nodes[-1]]
+        print('Best code with load', load, 'is:')
+        print(code)
