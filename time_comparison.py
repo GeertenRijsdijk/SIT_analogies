03e96df4ffa1c4addcc29f40e988860f9ed5a64a
Geerten Rijsdijk <geertenrijsdijk@hotmail.com>
2020-06-22 11:09:56 +0200

    added option to generate testfiles that can be used as input for the original PISA

--- time_comparison.py
+++ time_comparison.py
@@ -11,12 +11,15 @@ if __name__ == '__main__':
     error = False
     bf = True
     pisa = True
+    pisa_c = False
     if len(sys.argv) < 3:
         error = True
     if len(sys.argv) >= 4 and sys.argv[3] == 'F':
         bf = False
-    if len(sys.argv) == 5 and sys.argv[4] == 'F':
+    if len(sys.argv) >= 5 and sys.argv[4] == 'F':
         pisa = False
+    if len(sys.argv) >= 6 and sys.argv[5] == 'T':
+        pisa_c = True
     try:
         k = int(sys.argv[1])
         N = int(sys.argv[2])
@@ -25,12 +28,14 @@ if __name__ == '__main__':
     except:
         error = True
     if error:
-        print('USAGE: time_comparison.py <k> <N> <bf> <PISA>')
+        print('USAGE: time_comparison.py <k> <N> <bf> <PISA> <PISA_C>')
         print('k (positive int) = maximum length of string (required)')
         print('N (positive int) = \
 number of iterations average is taken over (required)')
         print('bf (T or F) = run brute-force compressor? (default T)')
         print('PISA (T or F) = run PISA compressor? (default T)')
+        print('PISA_C (T or F) = create testfiles for original PISA? \
+(default F)')
         quit()
 
 
@@ -38,13 +43,21 @@ number of iterations average is taken over (required)')
 
     bf_times = [0]
     pisa_times = [0]
+
+    strings = []
+    for n in range(N):
+        strings.append('')
+
     for l in range(1, k+1):
-
-        strings = []
-        for n in range(N):
-            s = ''.join([choice(['A','B','C', 'D']) for _ in range(l)])
-            strings.append(s)
-
+        for i, s in enumerate(strings):
+            s += choice(['A','B','C', 'D'])
+            strings[i] = s
+        # Create test files for original PISA speed comparison
+        if pisa_c:
+            with open('PISA_TESTS/test_' + str(l) + '.txt', 'w') as f:
+                for s in strings:
+                    f.write(s + '\n\n')
+        # Test brute-force compressor
         if bf:
             print(l, 'BF   :', end = ' ', flush = True)
             start = time.time()
@@ -54,7 +67,7 @@ number of iterations average is taken over (required)')
             t = (time.time() - start)/N
             bf_times.append(t)
             print('')
-
+        # Test PISA-based compressor
         if pisa:
             print(l, 'PISA :', end = ' ', flush = True)
             start = time.time()
@@ -72,7 +85,7 @@ number of iterations average is taken over (required)')
         plt.plot(x_axis, pisa_times, label = 'PISA-based')
 
     plt.xlabel('string length')
-    plt.ylabel('time needed for compression')
+    plt.ylabel('time needed for compression (seconds)')
     plt.legend()
 
     plt.show()
